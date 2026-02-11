2a7a92c78dbab00c650e5563f2873a2e66c9e9e2
Stanley <stanley@stanley.stanley>
2026-02-11 16:09:40 -0300

    Added media_notas.c

--- media_notas.c
+++ media_notas.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
 	
+	char name[50] = "";
 	float first_grade;
 	float second_grade;
 	float medium;
+
+	printf("What is your name? ");
+	fgets(name, sizeof(name), stdin);
+	name[strcspn(name, "\n")] = '\0';
 
 	printf("What was your first grade (0-10)? ");
 	scanf("%f", &first_grade);
@@ -15,10 +21,12 @@ int main(){
 	medium = (first_grade + second_grade) / 2;
 
 	if(medium > 7){
-		printf("You have a good use of your grades (your medium is higher than 7).\n");
+		printf("\nYour medium is a total of %.2f.\n", medium);
+		printf("%s, you had a good use of your grades.\n", name);
 	}else{
-		printf("You did not have a good use of your grades (your medium is not higher than 7).\n");
-	}
+		printf("\nYour medium is a total of %.2f.\n", medium);
+		printf("%s, you had a bad use of your grades.\n", name);
+	}	
 
 	return 0;
 }
